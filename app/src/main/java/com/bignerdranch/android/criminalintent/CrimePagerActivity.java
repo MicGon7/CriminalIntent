d75a264c9abdfe250fa4d88224e1d5d708729280
MichaelG <micgon7@gmail.com>
2018-03-08 14:04:12 -0500

    Completed Chapter 11 challenges: FIX CrimeFragment's Margins and ADD First and Last Buttons.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -9,6 +9,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,12 +22,16 @@ import java.util.UUID;
 
 
 public class CrimePagerActivity extends AppCompatActivity {
-
+    private static final String TAG = "CriminalIntent";
     private static final String EXTRA_CRIME_ID =
             "com.bignerdranch.android.criminalintent.crime_id";
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+    private Button mFirstCrimeButton;
+    private Button mLastCrimeButton;
+    private int currentItemPosition;
+
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimePagerActivity.class);
@@ -41,11 +48,36 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         mViewPager = findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
+        mFirstCrimeButton = findViewById(R.id.button_firstCrime);
+        mLastCrimeButton = findViewById(R.id.button_lastCrime);
         FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // FragmentStatePagerAdapter destroys the unneeded fragment.
+        // State refers to the fact that it will save out your fragment's Bundle
+        // from the onSaveInstanceState(Bundle) when it's destroyed.
+        // The new fragment will be restored using that instance state when the user
+        // navigates back. (memory frugal with memory)
+
+        // Fragment Pager Adapter never destroys the fragment but calls detach(Fragment) instead
+        // of remove(Fragment) call used by the FragmentStatePagerAdapter. So, the fragment instance
+        // remains alive in the FragmentManager. (Good for a small interface that has a fixed
+        // number of fragments - keep fragments in memory can make your controller code
+        // easier to manage)
+
+        handleButtonAccess();
+
+
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            // getItem only gets called when new fragment is created. x [] [] [] x (x = not created)
+
             @Override
             public Fragment getItem(int position) {
+                Log.d(TAG, "New Fragment Loaded @ " + position);
                 Crime crime = mCrimes.get(position);
+                currentItemPosition = position;
+
+
                 return CrimeFragment.newInstance(crime.getId());
             }
 
@@ -55,12 +87,67 @@ public class CrimePagerActivity extends AppCompatActivity {
             }
         });
 
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected Called." + mViewPager.getCurrentItem());
+                currentItemPosition = position;
+
+                handleButtonAccess();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+        mFirstCrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+                mFirstCrimeButton.setEnabled(false);
+                mLastCrimeButton.setEnabled(true);
+
+            }
+        });
+
+        mLastCrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Last Button Clicked!");
+                mViewPager.setCurrentItem(mCrimes.size() - 1);
+                mLastCrimeButton.setEnabled(false);
+                mFirstCrimeButton.setEnabled(true);
+
+            }
+        });
+
         // Setting the initial pager item.
-        for(int i = 0; i < mCrimes.size(); i++) {
-            if(mCrimes.get(i).getId().equals(crimeId)) {
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
                 mViewPager.setCurrentItem(i);
                 break;
             }
+        }
+
+    }
+
+    private void handleButtonAccess() {
+        if(currentItemPosition  != 0 || currentItemPosition != 100) {
+            mFirstCrimeButton.setEnabled(true);
+            mLastCrimeButton.setEnabled(true);
+        }
+        if (currentItemPosition == 0) {
+            mFirstCrimeButton.setEnabled(false);
+        } else if (currentItemPosition == 100) {
+            mLastCrimeButton.setEnabled(false);
         }
     }
 }
