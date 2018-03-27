93086ac0f75fbb1fcfd56e24bddb0bea8569ff39
MichaelG <micgon7@gmail.com>
2018-03-27 14:23:16 -0400

    Implicit Intents: Added funcitonality for selecting a subject from contacts and sending a crime report

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -3,10 +3,15 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -20,7 +25,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
-import java.text.SimpleDateFormat;
+import android.text.format.DateFormat;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -33,13 +39,15 @@ public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
-
     private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_CONTACT = 1;
 
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mSuspectButton;
+    private Button mReportButton;
 
 
     /* CrimeActivity will call this method when it needs to create a CrimeFragment, passing
@@ -113,7 +121,6 @@ public class CrimeFragment extends Fragment {
         });
 
 
-
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -122,6 +129,74 @@ public class CrimeFragment extends Fragment {
                 mCrime.setSolved(isChecked);
             }
         });
+
+        // TODO Read chapter to figure out what to do with these button events.
+        mSuspectButton = v.findViewById(R.id.button_crimeSuspect);
+        mSuspectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the implicit intent and pass it into startActivityForResult(...).
+                // Also show suspect name on button.
+
+
+            }
+        });
+        mReportButton = v.findViewById(R.id.button_crimeReport);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an implicit intent and pass it into startActivity(Intent).
+
+                // MANUAL WAY
+               // Intent i = new Intent(Intent.ACTION_SEND); // 1: Action
+//                i.setType("text/plain"); // 3: Data tpe.
+//                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+//                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.Crime_report_subject));
+//                i = Intent.createChooser(i, getString(R.string.send_report));
+
+                // RECOMMENDED WAY
+                Intent i =  ShareCompat.IntentBuilder.from(getActivity())
+                        .setChooserTitle(getString(R.string.send_report))
+                        .setSubject(getString(R.string.Crime_report_subject))
+                        .setText(getCrimeReport())
+                        .setType("text/plain")
+                        .getIntent();
+
+               i = Intent.createChooser(i, getString(R.string.send_report));
+
+
+                startActivity(i);
+            }
+        });
+
+        final Intent pickContact = new Intent(Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI);
+        //pickContact.addCategory(Intent.CATEGORY_HOME); (test no contact app functionality)
+        mSuspectButton = v.findViewById(R.id.button_crimeSuspect);
+        mSuspectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the implicit intent and pass it into startActivityForResult(...).
+                // Also show suspect name on button.
+
+                startActivityForResult(pickContact, REQUEST_CONTACT);
+
+            }
+        });
+
+        if (mCrime.getSuspect() != null) {
+            mSuspectButton.setText(mCrime.getSuspect());
+        }
+
+        // Avoid crash if contact app does not exist on device.
+        PackageManager packageManager = getActivity().getPackageManager();
+        if (packageManager.resolveActivity(pickContact,
+                PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            mSuspectButton.setEnabled(false);
+        }
+
+
+
 
         return v;
 
@@ -152,7 +227,7 @@ public class CrimeFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode != Activity.RESULT_OK) {
+        if (resultCode != Activity.RESULT_OK) {
             return;
         }
 
@@ -161,10 +236,63 @@ public class CrimeFragment extends Fragment {
             mCrime.setDate(date);
             updateDate();
 
+        } else if (requestCode == REQUEST_CONTACT && data != null) {
+            Uri contactUri = data.getData();
+            // Specify which fields you want your query to return values for.
+            String[] queryFields = new String[]{
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+            // Perform your query - the contactUri is like a "where" clause here.
+            Cursor c = getActivity().getContentResolver()
+                    .query(contactUri, queryFields,
+                            null,
+                            null,
+                            null);
+
+            try {
+                // Double-check that you actually got results
+                if (c.getCount() == 0) {
+                    return;
+                }
+
+                // Pull out the first column of the first row of data that is your suspect's name.
+                c.moveToFirst();
+                String suspect = c.getString(0);
+                mCrime.setSuspect(suspect);
+                mSuspectButton.setText(suspect);
+            } finally {
+                c.close();
+            }
         }
+
     }
 
     private void updateDate() {
         mDateButton.setText((mCrime.getDate().toString()));
+    }
+
+    // Creates four strings and then pieces them together and returns a complete report.
+    private String getCrimeReport() {
+        String solvedString = null;
+        if (mCrime.isSolved()) {
+            solvedString = getString(R.string.crime_report_solved);
+        } else {
+            solvedString = getString(R.string.crime_report_unsolved);
+        }
+
+        String dateFormat = "EEE, MMM dd, yyyy";
+        String dateString = DateFormat.format(dateFormat, mCrime.getDate()).toString();
+        String suspect = mCrime.getSuspect();
+        if (suspect == null) {
+            suspect = getString(R.string.crime_report_no_suspect);
+        } else {
+            //TODO Fix suspect value not showing up.
+            suspect = getString(R.string.crime_report_suspect);
+        }
+
+        String report = getString(R.string.crime_report,
+                mCrime.getTitle(), dateString, solvedString, suspect);
+
+        return report;
     }
 }
